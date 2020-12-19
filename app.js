75ab0b517e92f3f1fd256f9042a186eeb7ae1921
Armando <iglesias.armando@gmail.com>
2020-12-19 10:18:16 -0300

    products and cart

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
 
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,15 +20,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
-
-
+  User.findById("5fdb6e731b56161b249e4a7e")
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+  //next();
 });
 
 app.use('/admin', adminRoutes);
@@ -35,6 +35,7 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoConnect( () =>{
+  User.createAdminId();
   //console.log(mongoClient)
   app.listen(3000);
 } );
--- models/user.js
+++ models/user.js
@@ -1,16 +1,126 @@
-const Sequelize = require('sequelize');
+const getDb = require('../util/database').getDb;
+const mongoDb = require('mongodb');
 
-const sequelize = require('../util/database');
+class User{
+  constructor(username, email, cart, id ){
+    this.name = username;
+    this.email = email;
+    this.cart = cart;
+    this.id = id
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save(){
+    const db = getDb();
+    return db.collection('users').insertOne(this);
+  }
+
+  getCart(){
+    const db = getDb();
+
+    const productsId = this.cart.items.map( i => i.productId )
+
+    return db.collection('products').find({
+      _id : {
+        $in : productsId
+      }
+    }).toArray()
+    .then( products => {
+      return products.map( p =>{
+        return {
+          ...p,
+          quantity : this.cart.items.find( i => {
+            return i.productId.toString() === p._id.toString()
+          }).quantity
+        };
+      });
+    } );
+  
+  }
+
+
+  removeFromCart(productId){
+    const updatedCartItems = this.cart.items.filter( item => {
+      return item.productId.toString() !==  productId.toString();
+    } );
+    const db = getDb();
+    return db.collection('users').updateOne({
+      _id : new mongoDb.ObjectId(this.id)
+    }, {
+      $set : { 
+        cart: {
+          items : updatedCartItems
+        } 
+      }
+    }).then( result => console.log(result) )
+    .catch( err => console.error(err));
+
+
+  }
+
+  addToCart(product){
+    
+    let newQuantity = 1;
+    //this.cart = null;
+    if (this.cart == null) {
+      this.cart = {
+        items: []
+      };
+    }
+    const updatedCartItems = [...this.cart.items];
+    const cartProductIndex = this.cart.items.findIndex( cp => {
+      console.log(cp.productId, " ", product._id )  
+      return cp.productId.toString() === product._id.toString()
+     });
+
+    if (cartProductIndex != -1) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    }else{
+      updatedCartItems.push({
+        productId : new mongoDb.ObjectId(product._id),
+        quantity : newQuantity
+      });
+    }
+
+    const updatedCart = {
+      items: updatedCartItems
+    };
+    
+    const db = getDb();
+    return db.collection('users').updateOne({
+      _id : new mongoDb.ObjectId(this.id)
+    }, {
+      $set : { 
+        cart:  updatedCart
+      }
+    }).then( result => console.log(result) )
+    .catch( err => console.error(err));
+
+  }
+
+  static findById(userId){
+    const db = getDb();
+    return db.collection('users').findOne( { _id  : new mongoDb.ObjectId(userId) } )
+      .then( user => {
+        console.log("user" , user);
+        return user;
+      } )
+      .catch(err => console.error(err));
+  }
+
+  static createAdminId(){
+    const db = getDb();
+    return db.collection('users').findOne({ name : 'Admin' })
+      .then( user => {
+        if (!user) {
+          new User("Admin", "admin@test.com")
+            .save()
+            .then(newUser => console.log(newUser))
+            .catch( err => console.error(err) )
+        }
+      } )
+      .catch(err=> console.error(err) );
+  }
+}
 
 module.exports = User;
